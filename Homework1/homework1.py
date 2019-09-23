b374d91f4bc2f51ab9312b64f6949d62be6152e5
Aakhila Shaheen <shahe026@umn.edu>
2019-09-22 19:50:32 -0500

    Minor uodates

--- Homework1/homework1.py
+++ Homework1/homework1.py
@@ -3,14 +3,17 @@ import sys
 import csv
 
 def main():
+    total_query = 0
+    with open(r'query.fna','r') as q:
+        for line in q:
+            total_query += 1
+
     with open(r'output.txt', 'r') as f:
-        total_count = 0
-        percentage_match = 0;
+        percentage_match = 0
         bacterial_dict = {}
-        spamreader = csv.reader(f, delimiter='\t')
+        outputreader = csv.reader(f, delimiter='\t')
         total_similarity = 0
-        for row in spamreader:
-            total_count += 1
+        for row in outputreader:
             if (float(row[2]) >= 97):
                 total_similarity = total_similarity + float(row[2])
                 percentage_match+=1
@@ -20,12 +23,13 @@ def main():
             else:
                 bacterial_dict[name] = 1
 
-        fraction_match = float(percentage_match)/total_count
-        key,value = sorted(bacterial_dict.items(), key=lambda x:x[1], reverse=True)[0]
-        average_similarity = total_similarity / percentage_match 
+    fraction_match = float(percentage_match)/total_query
+    key,value = sorted(bacterial_dict.items(), key=lambda x:x[1], reverse=True)[0]
+    average_similarity = total_similarity / percentage_match 
                         
-        print "Percentage match >= 97:" , fraction_match
-        print "Most common species in the query is ", key.split("s__")[1], "with", value, "occurrences."
-        print "Average Similarity:" , average_similarity
+    print "Percentage match >= 97:" , fraction_match
+    print "Most common species in the query is ", key.split("s__")[1], "with", value, "occurrences."
+    print "Average Similarity:" , average_similarity
+
 if __name__ == '__main__':
     main()
