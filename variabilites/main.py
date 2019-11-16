73ee3f100fe545d5f6040a6f494789763c2ac200
Aakhila Shaheen <shahe026@umn.edu>
2019-11-16 13:41:01 -0600

    U

--- variabilites/main.py
+++ variabilites/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import utils
 import sys
 import collections
-
+import matplotlib.pyplot as plt
 
 '''
 calculate the variability
@@ -26,16 +26,39 @@ def get_variabilities(dictionary):
         if len(variability_per_region) > 0:
             # get the maximum count and divide by total count
             conserved = max(variability_per_region, key=variability_per_region.get)
-            all_variabilities.append(variability_per_region[conserved] / float(total_count))
+            all_variabilites.append(variability_per_region[conserved] / float(total_count))
         else:
-            all_variabilities.append(0)
-    return all_variabilities
+            all_variabilites.append(0)
+    return all_variabilites
+
+
+def plot_variabilities(variabilities):
+    x = np.arange(1, len(variabilities)+1)
+    y = np.array(variabilities)
+    # plt.plot(x,y)
+    # plt.show()
+
+
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+def plot_smooth_variabilites(variabilities):
+    y = np.array(variabilities)
+    y = moving_average(np.array(y), n=20)
+    x = np.arange(1, len(variabilities)+1)
+    plt.plot(x,y)
+    plt.show()    
 
 def main(filename):
     alignments = utils.read_fasta_file(filename)
     variabilities = get_variabilities(alignments)
     utils.write_variabilities(variabilities)
     print(len(variabilities))
+    plot_variabilities(variabilities)
+    plot_smooth_variabilites(variabilities)
 
 if __name__ == '__main__':
     main(sys.argv[1])
